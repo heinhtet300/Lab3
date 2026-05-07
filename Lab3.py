0dbed9474415a34294b934e77f84062a407bbc9a
HeinHtet <HEINZ.25@ichat.sp.edu.sg>
2026-05-07 15:11:12 +0800

    now

--- Lab3.py
+++ Lab3.py
@@ -11,8 +11,16 @@ def bubble_sort(arr, sorting_order):
 
     # Get number of elements in the list
     n = len(arr_result)
-
-    if n < 10:
+    check = True
+    for item in arr_result:
+        check = str(item).isdigit()
+        if check == 0:
+            break
+    if n == 0:
+        arr_result = 0
+    elif check == 0:
+        arr_result = 2
+    elif n < 10:
         # Traverse through all array elements
         for i in range(n - 1):
             # range(n) also work but outer loop will
@@ -34,7 +42,7 @@ def bubble_sort(arr, sorting_order):
                     # Return an empty array
                     arr_result = []
     else:
-        arr_result = -1
+        arr_result = 1
 
     return arr_result
 
