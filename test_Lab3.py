593b44f2f73fd40d6573586bb7790da9834dcd38
HeinHtet <HEINZ.25@ichat.sp.edu.sg>
2026-05-07 15:11:43 +0800

    hi

--- test_Lab3.py
+++ test_Lab3.py
@@ -28,3 +28,25 @@ def test_bubble_sort_invalid():
     result = Lab3.bubble_sort(input_arr, 3)
 
     assert (result == [])
+
+def test_bubble_sort_overten():
+    result = []
+    input_arr = [64, 34, 25, 12, 22, 11, 90,10,20,30,14]
+    result = Lab3.bubble_sort(input_arr, Lab3.SORT_ASCENDING)
+
+    assert (result == 1)
+
+def test_bubble_sort_zero():
+    result = []
+    input_arr = []
+    result = Lab3.bubble_sort(input_arr,Lab3.SORT_ASCENDING)
+    
+    assert(result == 0)
+
+def test_bubble_sort_notint():
+    result = []
+    input_arr = [64,32,25,"hi"]
+    result = Lab3.bubble_sort(input_arr,Lab3.SORT_ASCENDING)
+    
+    assert(result == 2)
+
